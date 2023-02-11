e0343eec5fb47f3629ab3d065d815bff71e6664f
ShriramGithub7 <93775361+ShriramGithub7@users.noreply.github.com>
2023-02-11 03:51:46 -0700

    Update dataTransform.py

--- utils/dataTransform.py
+++ utils/dataTransform.py
@@ -17,34 +17,33 @@ class Dataset():
                                            download=True)
 
 class AlbumentationImageDataset(Dataset):
-  def __init__(self, image_list, train=True):
-    self.image_list = image_list
-    self.augmented = A.Compose(
-                  {A.HorizontalFlip(),
-                   A.Normalize((0.49139968, 0.48215841, 0.44653091), (0.24703223, 0.24348513, 0.26158784)),
-                   A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.1,rotate_limit=45),
-                   A.CoarseDropout(max_holes=1, max_height=16, max_width=16, min_holes=1,min_height=16, min_width=16,
+    def __init__(self, image_list, train=True):
+        self.image_list = image_list
+        self.augmented = A.Compose({
+            A.HorizontalFlip(),
+            A.Normalize((0.49139968, 0.48215841, 0.44653091), (0.24703223, 0.24348513, 0.26158784)),
+            A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.1, rotate_limit=45),
+            A.CoarseDropout(max_holes=1, max_height=16, max_width=16, min_holes=1, min_height=16, min_width=16,
                              fill_value=np.mean([0.473363]), mask_fill_value=None),
-                   A.ToGray()
-                   })
-    self.norm = A.Normalize((0.49139968, 0.48215841, 0.44653091), (0.24703223, 0.24348513, 0.26158784))
-    self.train = train
+            A.ToGray()
+        })
+        self.norm = A.Normalize((0.49139968, 0.48215841, 0.44653091), (0.24703223, 0.24348513, 0.26158784))
+        self.train = train
   
-  def __len__(self):
-    return len(self.image_list)
+    def __len__(self):
+        return len(self.image_list)
   
-  def __getitem__(self, i):
-    image, label = self.image_list[i]
+    def __getitem__(self, i):
+        image, label = self.image_list[i]
     
-    if self.train:
-      #apply augumentation only for training
-      image=self.augmented(image=np.array(image))['image']
-    else:
-      image=self.norm(image=np.array(image))['image']
-    image=np.transpose(image, (2, 0, 1)).astype(np.float32)
+        if self.train:
+            # apply augmentation only for training
+            image = self.augmented(image=np.array(image))['image']
+        else:
+            image = self.norm(image=np.array(image))['image']
+        image = np.transpose(image, (2, 0, 1)).astype(np.float32)
     
-    return torch.totensor(image, dtype=torch.float), label
-   
+        return torch.tensor(image, dtype=torch.float), label   
 
 
 class DataLoader():
