245d36103232162c2fb04a0b6eee22fdb8de1feb
ShriramGithub7 <93775361+ShriramGithub7@users.noreply.github.com>
2023-02-11 04:03:08 -0700

    Update dataTransform.py

--- utils/dataTransform.py
+++ utils/dataTransform.py
@@ -26,7 +26,7 @@ class AlbumentationImageDataset(Dataset):
             A.Normalize(self.mean, (0.24703223, 0.24348513, 0.26158784)),
             A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.1, rotate_limit=45),
             A.CoarseDropout(max_holes=1, max_height=16, max_width=16, min_holes=1, min_height=16, min_width=16,
-                             fill_value=0.473363, mask_fill_value=None),
+                             fill_value=np.mean(mean), mask_fill_value=None),
             A.ToGray()
         })
         self.norm = A.Normalize(self.mean, (0.24703223, 0.24348513, 0.26158784))
