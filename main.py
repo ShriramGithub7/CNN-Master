d9cf0230a8874b95a96a1616286d288ece36fb55
ShriramGithub7 <93775361+ShriramGithub7@users.noreply.github.com>
2023-02-16 23:24:27 -0700

    Update main.py

--- main.py
+++ main.py
@@ -22,9 +22,9 @@ parser = argparse.ArgumentParser(description="Pytorch CIFAR10 Training")
 device= 'cuda' if torch.cuda.is_available() else 'cpu'
 
 class Main_customResNet:
-    def __init__(self, model, train_loader, test_loader, optimizer, l1, scheduler, num_epochs=20, ):
+    def __init__(self, model, train_loader, test_loader, optimizer, l1, scheduler, NUM_EPOCHS=20, ):
       self.model = model
-      self.num_epochs = num_epochs
+      self.NUM_EPOCHS = NUM_EPOCHS
       self.train_loader = train_loader
       self.test_loader = test_loader
       self.optimizer = optimizer
@@ -43,32 +43,32 @@ class Main_customResNet:
         data, target = data.to(device), target.to(device)
 
         # Init
-        optimizer.zero_grad()
+        self.optimizer.zero_grad()
         # In PyTorch, we need to set the gradients to zero before starting to do backpropragation because PyTorch 
         # accumulates the gradients on subsequent backward passes. Because of this, when you start your training loop, 
         # ideally you should zero out the gradients so that you do the parameter update correctly.
 
         # Predict
-        y_pred = model(data)
+        y_pred = self.model(data)
 
         # Calculate loss
         loss = F.nll_loss(y_pred, target)
         l1 = 0
         lambda_l1 = 0.01
         if self.l1:
-          for p in model.parameter():
+          for p in self.model.parameter():
             self.l1 = self.l1 + p.abs().sum()
 
         loss = loss + lambda_l1*self.l1
 
         # Backpropagation
         loss.backward()
-        optimizer.step()
+        self.optimizer.step()
 
         train_loss += loss.item()
 
         # Update LR
-        scheduler.step()
+        self.scheduler.step()
 
         # Update pbar-tqdm
 
@@ -82,13 +82,13 @@ class Main_customResNet:
       return 100*correct/processed, train_loss/num_loops
 
     def test(self):
-        model.eval()
+        self.model.eval()
         test_loss = 0
         correct = 0
         with torch.no_grad():
             for data, target in self.test_loader:
                 data, target = data.to(device), target.to(device)
-                output = model(data)
+                output = self.model(data)
                 test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
                 pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
                 correct += pred.eq(target.view_as(pred)).sum().item()
@@ -102,35 +102,35 @@ class Main_customResNet:
 
         return 100. * correct / len(self.test_loader.dataset), test_loss
 
-    #def fit_model(net, train_data, test_data, num_epochs=20, l1=False, l2=False):
+    #def fit_model(net, train_data, test_data, self.num_epochs=24, l1=False, l2=False):
     def fit_model(self):
       training_acc, training_loss, testing_acc, testing_loss = [], [], [], []
 
       if l2:
-        optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9, weight_decay=1e-4)
+        self.optimizer = optim.SGD(self.model.parameters(), lr=0.001, momentum=0.9, weight_decay=1e-4)
       else:
-        optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+        self.optimizer = optim.SGD(self.model.parameters(), lr=0.001, momentum=0.9)
 
       # set LRMAX and LRMIN
       max_lr = 0.02
       min_lr = max_lr/10
 
       # set One Cycle Policy scheduler
-      num_steps = NUM_EPOCHS * len(self.train_loader)
+      num_steps = self.NUM_EPOCHS * len(self.train_loader)
       anneal_strategy = 'cos'
       cycle_momentum = True
       max_momentum = 0.95
       base_momentum = 0.85
       step_size_up = int(num_steps * 0.3)
       step_size_down = num_steps - step_size_up
-      scheduler = optim.lr_scheduler.OneCycleLR(optimizer, max_lr=max_lr, total_steps=num_steps, anneal_strategy=anneal_strategy, cycle_momentum=cycle_momentum, max_momentum=max_momentum, base_momentum=base_momentum, div_factor=max_lr/min_lr, pct_start=step_size_up/num_steps, steps_per_epoch=len(train_loader))
+      scheduler = optim.lr_scheduler.OneCycleLR(self.optimizer, max_lr=max_lr, total_steps=num_steps, anneal_strategy=anneal_strategy, cycle_momentum=cycle_momentum, max_momentum=max_momentum, base_momentum=base_momentum, div_factor=max_lr/min_lr, pct_start=step_size_up/num_steps, steps_per_epoch=len(train_loader))
 
-      for epoch in range(1,NUM_EPOCHS+1):
+      for epoch in range(1,self.NUM_EPOCHS+1):
           print("EPOCH:", epoch)
 
           # update LRMAX at epoch 5
           if epoch == 5:
-              self.scheduler = optim.lr_scheduler.OneCycleLR(optimizer, max_lr=max_lr, total_steps=num_steps, anneal_strategy=anneal_strategy, cycle_momentum=cycle_momentum, max_momentum=max_momentum, base_momentum=base_momentum, div_factor=max_lr/min_lr, pct_start=step_size_up/num_steps, steps_per_epoch=len(train_loader))
+              self.scheduler = optim.lr_scheduler.OneCycleLR(self.optimizer, max_lr=max_lr, total_steps=num_steps, anneal_strategy=anneal_strategy, cycle_momentum=cycle_momentum, max_momentum=max_momentum, base_momentum=base_momentum, div_factor=max_lr/min_lr, pct_start=step_size_up/num_steps, steps_per_epoch=len(train_loader))
 
           train_acc, train_loss = train(self.model, self.device, self.train_loader, self.optimizer, self.l1, self.scheduler)
           test_acc, test_loss = test(self.model, self.device, self.test_loader)
@@ -140,7 +140,7 @@ class Main_customResNet:
           testing_acc.append(test_acc)
           testing_loss.append(test_loss)
 
-      return net, (training_acc, training_loss, testing_acc, testing_loss)
+      return self.model, (training_acc, training_loss, testing_acc, testing_loss)
 
 
 best_acc = 0
